cc69109febb2f70b8b6c4f4ca010f0632bc37916
Sonja Heinze <sonjaleaheinze@gmail.com>
2023-06-27 20:51:14 +0200

    First try

---
+++ .github/scripts/setup-irmin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+git clone https://github.com/mirage/irmin.git
+cd irmin
+git checkout 3.7.0
+sudo apt install -y gnuplot-x11 libgmp-dev pkg-config libffi-dev
+opam install . --deps-only --with-test
+opam exec -- dune build @check
